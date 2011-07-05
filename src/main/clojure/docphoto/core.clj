3abc82ad6ac0a191ef8754859d42f9e896636140
Robert Marianski <rob@marianski.com>
2011-07-05 15:10:26 -0400

    attempt to build on top of flutter for forms

--- src/main/clojure/docphoto/core.clj
+++ src/main/clojure/docphoto/core.clj
@@ -4,7 +4,10 @@
         [ring.middleware.multipart-params :only (wrap-multipart-params)]
         [hiccup.page-helpers :only (xhtml)]
         [ring.adapter.jetty-servlet :only (run-jetty)]
-        [formidable.core :only (render)]
+        [flutter.html4 :only (html4-fields)]
+        [flutter.shortcuts :only (wrap-shortcuts)]
+        [flutter.params :only (wrap-params)]
+        [formidable.core :only (wrap-form-field wrap-errors)]
         [ring.util.response :only (redirect)]
         [decline.core :only
          (validations validation validate-val validate-some)])
@@ -34,12 +37,6 @@
                    (or (user-from-session request)
                        "anonymous"))]]))
 
-(defmethod render :password
-  [field value]
-  [:input {:type :password
-           :name (:name field)
-           :value (or value "")}])
-
 (defmacro validate-vals [& val-data]
   `(validations
     ~@(for [[k f error] (partition 3 val-data)]
@@ -59,36 +56,36 @@
             [[FirstName = username]
              [Password = password]]))
 
-(defn render-login-form
-  ([params] (render-login-form params {}))
-  ([params errors]
-     (xhtml
-      [:div
-       [:h1 "Login"]
-       (render {:type :uniform
-                :fields
-                [{:type :text :name :username :error (:username errors)}
-                 {:type :password :name :password :error (:password errors)}]
-                :buttons [{:type :submit :value :login}]}
-               params)])))
+;; (defn render-login-form
+;;   ([params] (render-login-form params {}))
+;;   ([params errors]
+;;      (xhtml
+;;       [:div
+;;        [:h1 "Login"]
+;;        (render {:type :uniform
+;;                 :fields
+;;                 [{:type :text :name :username :error (:username errors)}
+;;                  {:type :password :name :password :error (:password errors)}]
+;;                 :buttons [{:type :submit :value :login}]}
+;;                params)])))
 
-(defn login [request user]
-  (doto (:session request)
-    (.setAttribute "userid" (:id user))
-    (.setAttribute "user" user)))
+;; (defn login [request user]
+;;   (doto (:session request)
+;;     (.setAttribute "userid" (:id user))
+;;     (.setAttribute "user" user)))
 
-(defn login-view [request]
-  (let [params (:params request)]
-    (if (= (:request-method request) :post)
-      (if-let [errors ((login-validator) params)]
-        (render-login-form params errors)
-        (let [{:keys [username password]} params]
-          (if-let [user (query-login-info conn username password)]
-            (do
-              (login request (sf/sobject->map user))
-              (redirect "/"))
-            (render-login-form params {:username "Invalid Credentials"}))))
-      (render-login-form params))))
+;; (defn login-view [request]
+;;   (let [params (:params request)]
+;;     (if (= (:request-method request) :post)
+;;       (if-let [errors ((login-validator) params)]
+;;         (render-login-form params errors)
+;;         (let [{:keys [username password]} params]
+;;           (if-let [user (query-login-info conn username password)]
+;;             (do
+;;               (login request (sf/sobject->map user))
+;;               (redirect "/"))
+;;             (render-login-form params {:username "Invalid Credentials"}))))
+;;       (render-login-form params))))
 
 (defn userinfo-view [request]
   (xhtml
@@ -96,76 +93,75 @@
     (for [[k v] (.getAttribute (:session request) "user")]
       (list [:dt k] [:dd v]))]))
 
-(defn render-register-form
-  ([params] (render-register-form params {}))
-  ([params errors]
-     (xhtml
-      [:div
-       [:h1 "Register"]
-       (render {:type :uniform
-                :fields
-                [{:type :text :name :username :error (:username errors)}
-                 {:type :password :name :password :error (:password errors)}
-                 {:type :password :name :password2
-                  :label "Password" :error (:password2 errors)}
-                 {:type :text :name :first-name
-                  :label "First name" :error (:first-name errors)}
-                 {:type :text :name :last-name
-                  :label "Last name" :error (:last-name errors)}
-                 {:type :text :name :email :error (:email errors)}
-                 {:type :text :name :phone :error (:phone errors)}]
-                :buttons [{:type :submit :value "Register"}]}
-               params)])))
+;; (defn render-register-form
+;;   ([params] (render-register-form params {}))
+;;   ([params errors]
+;;      (xhtml
+;;       [:div
+;;        [:h1 "Register"]
+;;        (render {:type :uniform
+;;                 :fields
+;;                 [{:type :text :name :username :error (:username errors)}
+;;                  {:type :password :name :password :error (:password errors)}
+;;                  {:type :password :name :password2
+;;                   :label "Password" :error (:password2 errors)}
+;;                  {:type :text :name :first-name
+;;                   :label "First name" :error (:first-name errors)}
+;;                  {:type :text :name :last-name
+;;                   :label "Last name" :error (:last-name errors)}
+;;                  {:type :text :name :email :error (:email errors)}
+;;                  {:type :text :name :phone :error (:phone errors)}]
+;;                 :buttons [{:type :submit :value "Register"}]}
+;;                params)])))
 
-(defn register-validator []
-  (validations
-   (validate-val :Username not-empty {:Username :required})
-   (validate-val :Password not-empty {:Password :required})
-   (validate-val :Email not-empty {:Email :required})
-   (validate-val :Phone not-empty {:Phone :required})
-   (validate-val :FirstName not-empty {:FirstName :required})
-   (validate-val :LastName not-empty {:LastName :required})))
+;; (defn register-validator []
+;;   (validations
+;;    (validate-val :Username not-empty {:Username :required})
+;;    (validate-val :Password not-empty {:Password :required})
+;;    (validate-val :Email not-empty {:Email :required})
+;;    (validate-val :Phone not-empty {:Phone :required})
+;;    (validate-val :FirstName not-empty {:FirstName :required})
+;;    (validate-val :LastName not-empty {:LastName :required})))
 
-(defn register-view [request]
-  (let [params (:params request)]
-    (if (= (:request-method request) :post)
-      (if-let [errors ((register-validator) params)]
-        (render-register-form params errors)
-        (let [{username :Username password :Password
-               first-name :FirstName last-name :LastName
-               email :Email phone :Phone}
-              params]
-          (sf/create-contact
-           conn
-           {:FirstName first-name
-            :LastName last-name
-            :Email email
-            :Phone phone
-            :UserName__c username
-            :Password__c (md5 password)})
-          (redirect "/userinfo")))
-      (render-register-form params))))
+;; (defn register-view [request]
+;;   (let [params (:params request)]
+;;     (if (= (:request-method request) :post)
+;;       (if-let [errors ((register-validator) params)]
+;;         (render-register-form params errors)
+;;         (let [{username :Username password :Password
+;;                first-name :FirstName last-name :LastName
+;;                email :Email phone :Phone}
+;;               params]
+;;           (sf/create-contact
+;;            conn
+;;            {:FirstName first-name
+;;             :LastName last-name
+;;             :Email email
+;;             :Phone phone
+;;             :UserName__c username
+;;             :Password__c (md5 password)})
+;;           (redirect "/userinfo")))
+;;       (render-register-form params))))
 
-(defn- make-field-stanza
-  ([fieldinfo errors-symbol]
-     (if (keyword? fieldinfo)
-       (make-field-stanza
-        fieldinfo (string/capitalize (name fieldinfo)) errors-symbol)
-       (make-field-stanza
-        (first fieldinfo) (second fieldinfo) errors-symbol)))
-  ([field label errors-symbol]
-     `{:type :text :name ~field :label ~label :error (~field ~errors-symbol)}))
+(defn- make-field-stanza [field fieldinfo]
+  `(~field ~@fieldinfo))
 
 (defn- make-form-render-fn [fields options]
-  (let [errors (gensym "errors__")]
-    `(fn [params# ~errors]
-       (xhtml
-        (render {:type :uniform
-                 :fields
-                 [~@(map #(make-field-stanza % errors) fields)]
-                 :buttons [{:type :submit
-                            :value ~(:submit-label options "Submit")}]}
-                params#)))))
+  (let [field (gensym "field__")]
+    `(fn [params# errors#]
+       (let [~field (->
+                     html4-fields
+                     wrap-form-field
+                     wrap-shortcuts
+                     (wrap-errors errors#)
+                     (wrap-params params#))]
+         (xhtml
+          [:form.uniForm {:method :post :action "."}
+           [:p "Register!"]
+           [:fieldset
+            ~@(map #(make-field-stanza field %) fields)]
+           [:input {:type :submit
+                    :value "Register"}]])))))
 
 (defn- make-validator-stanza [fieldinfo]
   (if (keyword? fieldinfo)
@@ -204,13 +200,13 @@
   (println "registered:" register-map))
 
 (defformpage register-page
-  [[:userName__c "Username"]
-   [:password__c "Password"]
-   [:password2 "Password"]
-   [:firstName "First Name"]
-   [:lastName "Last Name"]
-   :email
-   :phone]
+  [[:text {} :userName__c {:label "Username"}]
+   [:password {} :password__c {:label "Password"}]
+   [:password {} :password2 {:label "Password"}]
+   [:text {} :firstName {:label "First Name"}]
+   [:text {} :lastName {:label "Last Name"}]
+   [:text {} :email {:label "Email"}]
+   [:text {} :phone {:label "Phone"}]]
   [{render-form-fn :render params :params}]
   (let [{password1 :password__c password2 :password2} params]
     (if (not (= password1 password2))
@@ -221,9 +217,9 @@
 
 (defroutes main-routes
   (GET "/" [] home-view)
-  (ANY "/login" [] login-view)
+;  (ANY "/login" [] login-view)
   (ANY "/userinfo" [] userinfo-view)
-  (ANY "/register" [] register-view)
+;  (ANY "/register" [] register-view)
   (ANY "/macro-register" [] register-page)
   (route/files "/public")
   (route/not-found "Page not found"))
