bc101775201a28919a9ba0887220e1eabf244b5d
Robert Marianski <rob@marianski.com>
2011-07-05 20:23:11 -0400

    plugin login/logout views

--- src/main/clojure/docphoto/core.clj
+++ src/main/clojure/docphoto/core.clj
@@ -10,14 +10,15 @@
         [formidable.core :only (wrap-form-field wrap-errors)]
         [ring.util.response :only (redirect)]
         [decline.core :only
-         (validations validation validate-val validate-some)])
+         (validations validation validate-val validate-some)]
+        [clojure.contrib.core :only (-?>)])
   (:require [compojure.route :as route]
             [docphoto.salesforce :as sf]
             [clojure.contrib.string :as string])
   (:import [org.apache.commons.codec.digest DigestUtils]))
 
 ;; global salesforce connection
-(defn conn)
+(defonce conn nil)
 
 (defn md5 [s] (DigestUtils/md5Hex s))
 
@@ -25,126 +26,17 @@
   (fn [request]
     (handler
      (if-let [servlet-request (:servlet-request request)]
-       (assoc request :session (.getSession servlet-request))
+       (assoc request :session (.getSession servlet-request true))
        request))))
 
-(defn user-from-session [request]
-  (.getAttribute (:session request) "userid"))
+(defn session-get-user [request]
+  (.getAttribute (:session request) "user"))
 
-(defn home-view [request]
-  (xhtml [:div [:h1 "hi world"]
-          [:p (str "user: "
-                   (or (user-from-session request)
-                       "anonymous"))]]))
+(defn session-save-user [request user]
+  (.setAttribute (:session request) "user" user))
 
-(defmacro validate-vals [& val-data]
-  `(validations
-    ~@(for [[k f error] (partition 3 val-data)]
-       `(validate-val ~k ~f {~k ~error}))))
-
-(defn login-validator []
-  (validate-vals :username not-empty :required
-                 :password not-empty :required))
-
-;; XXX explore strategy of matching salesforce names exactly
-;; that way we don't have to adapt keys, just select them
-;; although if we're selecting them, might as well just rename them
-
-(defn query-login-info [conn username password]
-  (sf/query conn Contact
-            [Id UserName__c FirstName LastName Email Phone]
-            [[FirstName = username]
-             [Password = password]]))
-
-;; (defn render-login-form
-;;   ([params] (render-login-form params {}))
-;;   ([params errors]
-;;      (xhtml
-;;       [:div
-;;        [:h1 "Login"]
-;;        (render {:type :uniform
-;;                 :fields
-;;                 [{:type :text :name :username :error (:username errors)}
-;;                  {:type :password :name :password :error (:password errors)}]
-;;                 :buttons [{:type :submit :value :login}]}
-;;                params)])))
-
-;; (defn login [request user]
-;;   (doto (:session request)
-;;     (.setAttribute "userid" (:id user))
-;;     (.setAttribute "user" user)))
-
-;; (defn login-view [request]
-;;   (let [params (:params request)]
-;;     (if (= (:request-method request) :post)
-;;       (if-let [errors ((login-validator) params)]
-;;         (render-login-form params errors)
-;;         (let [{:keys [username password]} params]
-;;           (if-let [user (query-login-info conn username password)]
-;;             (do
-;;               (login request (sf/sobject->map user))
-;;               (redirect "/"))
-;;             (render-login-form params {:username "Invalid Credentials"}))))
-;;       (render-login-form params))))
-
-(defn userinfo-view [request]
-  (xhtml
-   [:dl
-    (for [[k v] (.getAttribute (:session request) "user")]
-      (list [:dt k] [:dd v]))]))
-
-;; (defn render-register-form
-;;   ([params] (render-register-form params {}))
-;;   ([params errors]
-;;      (xhtml
-;;       [:div
-;;        [:h1 "Register"]
-;;        (render {:type :uniform
-;;                 :fields
-;;                 [{:type :text :name :username :error (:username errors)}
-;;                  {:type :password :name :password :error (:password errors)}
-;;                  {:type :password :name :password2
-;;                   :label "Password" :error (:password2 errors)}
-;;                  {:type :text :name :first-name
-;;                   :label "First name" :error (:first-name errors)}
-;;                  {:type :text :name :last-name
-;;                   :label "Last name" :error (:last-name errors)}
-;;                  {:type :text :name :email :error (:email errors)}
-;;                  {:type :text :name :phone :error (:phone errors)}]
-;;                 :buttons [{:type :submit :value "Register"}]}
-;;                params)])))
-
-;; (defn register-validator []
-;;   (validations
-;;    (validate-val :Username not-empty {:Username :required})
-;;    (validate-val :Password not-empty {:Password :required})
-;;    (validate-val :Email not-empty {:Email :required})
-;;    (validate-val :Phone not-empty {:Phone :required})
-;;    (validate-val :FirstName not-empty {:FirstName :required})
-;;    (validate-val :LastName not-empty {:LastName :required})))
-
-;; (defn register-view [request]
-;;   (let [params (:params request)]
-;;     (if (= (:request-method request) :post)
-;;       (if-let [errors ((register-validator) params)]
-;;         (render-register-form params errors)
-;;         (let [{username :Username password :Password
-;;                first-name :FirstName last-name :LastName
-;;                email :Email phone :Phone}
-;;               params]
-;;           (sf/create-contact
-;;            conn
-;;            {:FirstName first-name
-;;             :LastName last-name
-;;             :Email email
-;;             :Phone phone
-;;             :UserName__c username
-;;             :Password__c (md5 password)})
-;;           (redirect "/userinfo")))
-;;       (render-register-form params))))
-
-(defn register [register-map]
-  (println "registered:" register-map))
+(defn session-delete [request]
+  (.invalidate (:session request)))
 
 (defn- make-fields-render-fn [fields options]
   (let [field (gensym "field__")]
@@ -172,10 +64,11 @@
   `(validations
     ~@(keep make-validator-stanza fields)))
 
-(defn make-form-render-fn [binding fields-render-fn form-body options]
+(defn make-form-render-fn [request binding fields-render-fn form-body options]
   `(fn [params# errors#]
      (let [~@binding {:render-fields ~fields-render-fn
                       :params params#
+                      :request ~request
                       :errors errors#}]
        ~form-body)))
 
@@ -193,12 +86,13 @@
            params (gensym "params__")
            request (gensym "request__")]
        `(let [~fields-render-fn ~(make-fields-render-fn fields options)
-              ~form-render-fn ~(make-form-render-fn
-                                field-render-bindings fields-render-fn
-                                form-render-body options)
               ~validator-fn ~(make-form-validator-fn fields)]
           (defn ~fn-name [~request]
-            (let [~params (:params ~request)]
+            (let [~params (:params ~request)
+                  ~form-render-fn ~(make-form-render-fn
+                                    request
+                                    field-render-bindings fields-render-fn
+                                    form-render-body options)]
               (if (= (:request-method ~request) :post)
                 (if-let [errors# (~validator-fn ~params)]
                   (~form-render-fn ~params errors#)
@@ -208,7 +102,53 @@
                     ~success-body))
                 (~form-render-fn ~params {}))))))))
 
-(defformpage register-page
+(defn home-view [request]
+  (xhtml [:div [:h1 "hi world"]
+          [:p (str "user: "
+                   (or (-?> (session-get-user request) :firstName)
+                       "anonymous"))]]))
+
+(defmacro validate-vals [& val-data]
+  `(validations
+    ~@(for [[k f error] (partition 3 val-data)]
+       `(validate-val ~k ~f {~k ~error}))))
+
+(defn userinfo-view [request]
+  (xhtml
+   [:dl
+    (for [[k v] (.getAttribute (:session request) "user")]
+      (list [:dt k] [:dd v]))]))
+
+(defn register [register-map]
+  (println "registered:" register-map))
+
+(defn login [request user]
+  (session-save-user request user))
+
+(defn logout [request]
+  (session-delete request))
+
+(defformpage login-view
+  [{:field [:text {} :userName__c {:label "Username"}] :validator {:fn not-empty :msg :required}}
+   {:field [:password {} :password__c {:label "Password"}] :validator {:fn not-empty :msg :required}}]
+  [{:keys [render-fields params errors]}]
+  (xhtml
+   [:form {:method :post :action "/login"}
+    [:h2 "Login"]
+    (render-fields params errors)
+    [:input {:type :submit :value "Login"}]])
+  [{render-form-fn :render-form params :params request :request}]
+  (if-let [user (first
+                 (sf/query conn Contact
+                           [Id UserName__c FirstName LastName Email Phone]
+                           [[userName__c = (:userName__c params)]
+                            [password__c = (md5 (:password__c params))]]))]
+    (do (login request user) (redirect "/userinfo"))
+    (render-form-fn params {:userName__c "Invalid Credentials"})))
+
+(defn logout-view [request] (logout request) (redirect "/"))
+
+(defformpage register-view
   [{:field [:text {} :userName__c {:label "Username"}] :validator {:fn not-empty :msg :required}}
    {:field [:password {} :password__c {:label "Password"}] :validator {:fn not-empty :msg :required}}
    {:field [:password {} :password2 {:label "Password"}] :validator {:fn not-empty :msg :required}}
@@ -232,10 +172,10 @@
 
 (defroutes main-routes
   (GET "/" [] home-view)
-;  (ANY "/login" [] login-view)
-  (ANY "/userinfo" [] userinfo-view)
-;  (ANY "/register" [] register-view)
-  (ANY "/macro-register" [] register-page)
+  (GET "/userinfo" [] userinfo-view)
+  (ANY "/login" [] login-view)
+  (GET "/logout" [] logout-view)
+  (ANY "/register" [] register-view)
   (route/files "/public")
   (route/not-found "Page not found"))
 
