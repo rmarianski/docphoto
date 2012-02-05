8944edd3d3613a0e623d6971a6abaa6bf000cf7f
Robert Marianski <rob@marianski.com>
2012-02-05 18:52:56 -0500

    specify clj stacktrace version to work around error on server

--- project.clj
+++ project.clj
@@ -16,4 +16,5 @@
                  [flutter "0.0.7"]
                  [org.imgscalr/imgscalr-lib "4.1"]
                  [com.draines/postal "1.7.0"]
-                 [org.clojars.mccraigmccraig/core.incubator "0.1.1-SNAPSHOT"]])
+                 [org.clojars.mccraigmccraig/core.incubator "0.1.1-SNAPSHOT"]
+                 [clj-stacktrace "0.2.4"]])
