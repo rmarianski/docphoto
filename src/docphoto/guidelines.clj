ce9698e3194c22d2434eb6a3ba9801e6944fed72
Robert Marianski <rob@marianski.com>
2012-03-02 19:15:43 +0000

    updated moving walls 20 guidelines

--- src/docphoto/guidelines.clj
+++ src/docphoto/guidelines.clj
@@ -22,18 +22,18 @@
            [:p "The Open Society Foundations invite photographers to submit a body of work for consideration in the Moving Walls 20 group exhibition, scheduled to open in New York in early 2013."]
            [:p "The Moving Walls exhibition series showcases documentary photography that highlights human rights and social issues that coincide with the Open Society Foundations' mission. Moving Walls is exhibited at our offices in New York and Washington, D.C. "]
            [:p "Launched in 1998, Moving Walls has featured over 175 photographers. Over the past 14 years, we have been proud to support the brave and difficult (and often self-funded) work that photographers undertake globally in their visual documentation of complex social and political issues. Their images provide the world with evidence of human rights abuses, put faces onto a conflict, document the struggles and defiance of marginalized people, reframe how issues are discussed publicly, and provide opportunities for reflection and discussion. Moving Walls honors this work while visually highlighting the Open Society Foundations' mission to staff and visitors."]
-           [:p " For participating photographers, a key benefit of the program is to gain exposure for their projects, as well as the social justice or human rights issues they address. In addition to a $2000 honorarium, photographers receive their professionally-produced exhibitions at the end of the exhibition tour in NY and Washington, D.C."]
-           [:p "With this next competition, Moving Walls will celebrate its 20th exhibition cycle. Moving Walls 20 will also be the inaugural exhibition in the Open Society Foundations' new headquarters on West 57th Street in New York. We intend to showcase three to five discrete bodies of work, depending on the scope of the selected projects."]
+           [:p " For participating photographers, a key benefit of the program is to gain exposure for their projects, as well as the social justice or human rights issues they address. In addition to a $2500 honorarium, photographers receive their professionally-produced exhibitions at the end of the exhibition tour in NY and Washington, D.C."]
+           [:p "With this next competition, Moving Walls will celebrate its 20th exhibition cycle. Moving Walls 20 will also be the inaugural exhibition in the Open Society Foundations' new headquarters on West 57th Street in New York. We intend to showcase three to five discrete bodies of work."]
            [:p "To view images from our recent exhibitions, please visit: " (link-to "http://www.movingwalls.org" "www.movingwalls.org") "."]
            [:h2 "Areas of Interest"]
            [:p "Each Moving Walls exhibit highlights issues or geographic regions where the Open Society Foundations are active. Priority is given to work whose subject has not been recently addressed in Moving Walls, and special consideration is given to long-term work produced over years of commitment to an issue or community. Work in progress may be submitted as long as a substantial portion of the work has been completed."]
 
-           [:p "Listed below are some focus areas for the Open Society Foundations, and examples of specific topics about which we are interested in receiving submissions. Please note that photographers are welcome to submit their work for Moving Walls even if their subject area is not included on this list. All work submitted will be considered for exhibition. In addition to the focus areas listed below, please visit our website (" (link-to "http://www.soros.org/" "www.soros.org") ") for a complete listing of priorities and programs"]
+           [:p "Listed below are some focus areas for the Open Society Foundations, and examples of specific topics about which we are interested in receiving submissions. " [:strong "Please note that photographers are welcome to submit their work for Moving Walls even if their subject area is not included on this list."] " All work submitted will be considered for exhibition. In addition to the focus areas listed below, please visit our website (" (link-to "http://www.soros.org/" "www.soros.org") ") for a complete listing of priorities and programs."]
 
            [:div.interest-areas
             (interest-list "Justice"
                            "Pretrial detention"
-                           "Detention of immigrants, especially in the United States")
+                           "Detention of immigrants")
 
             (interest-list "Public health"
                            "Public health issues in Africa, including access to essential medicines, access to health care, palliative care"
@@ -50,7 +50,7 @@
                            "Creation of South Sudan")
 
             (interest-list "Economic and Racial Justice"
-                           "Economic downturn in the United States, including the foreclosure crisis"
+                           "Economic downturn in the United States"
                            "Images that reframe mainstream media representations of African American men and boys")
 
             (interest-list "Women"
@@ -60,7 +60,7 @@
                            "Youth movements, especially political participation in voter registration, policy reform efforts, public education, especially in Eastern Europe")
 
             (interest-list "Climate Change"
-                           "Impact of climate change on communities")]
+                           "The human costs of climate change")]
 
            [:h2 "Who Can Apply"]
 
@@ -94,7 +94,7 @@
 
            [:p "You will be asked to complete or upload the following:"]
            [:ol
-            [:li [:strong "contact information and summary of project"]]
+            [:li [:strong "a project summary"] " (50 words maximum)"]
             [:li [:strong "a project statement*"] " (600 words maximum) describing the project you would like to exhibit;"]
             [:li [:strong "a short narrative bio"] " (250 words maximum) summarizing your previous work and experience;"]
             [:li [:strong "a summary of your engagement"] " with the story or issue (600 words maximum). Please respond to the following questions:"
@@ -114,10 +114,10 @@
            [:p "Complete submissions must be " [:u "received"] " via the online application system (" (link-to "http://apply.movingwalls.org/exhibit/mw20/apply" "apply.movingwalls.org") ") by " [:strong "5pm (Eastern Standard Time) on Thursday, April 19, 2012. Please do not wait until immediately prior to the deadline to submit work."] " Due to intake of a high volume of large files, we occasionally experience technical difficulties in the days leading up to the deadline. Please help us to avoid this by submitting early."]
 
            [:h2 "Review and Selection Process"]
-           [:p [:strong "Phase 1"] ": Applications are reviewed and selected by a committee of foundation staff with expertise in various program areas, and by curators Susan Meiselas and Stuart Alexander. In evaluating the work, we consider the quality of the photographs and their relevance to the Open Society Foundations' overall mission and activities. The committee also aims to select a diversity of issues and geographic areas in order to avoid repetition of topics shown in recent exhibitions. Past exhibitions can be viewed at: www.movingwalls.org."]
-           [:p "We receive 200-400 submissions each round. For Moving Walls 20, we plan to select three to five bodies of work."]
-
-           [:p [:strong "Phase 2"] ": Selected photographers will be designated wall space and encouraged to visit our office in New York to meet with our curators and prepare installation plans. Travel to our New York office for curatorial meetings is not part of Moving Walls payment and is not a requirement. Photographers who are unable to travel to New York may correspond with our curators by e-mail and submit their installation plans electronically. "]
+           [:p [:strong "Phase 1"] ": The entire body of 200-400 submissions are reviewed by Documentary Photography Project staff, who create a shortlist of applicants."]
+           [:p [:strong "Phase 2"] ": Shortlisted applications are reviewed and selected by a committee of foundation staff with expertise in various program areas, and by curators Susan Meiselas and Stuart Alexander. In evaluating the work, we consider the quality of the photographs and their relevance to the Open Society Foundations’ overall mission and activities. The committee also aims to select a diversity of issues and geographic areas in order to avoid repetition of topics shown in recent exhibitions. Past exhibitions can be viewed at: " (link-to "http://www.movingwalls.org/" "www.movingwalls.org") "."]
+           [:p "For Moving Walls 20, we plan to select three to five bodies of work."]
+           [:p [:strong "Phase 3"] ": Selected photographers will be designated wall space and encouraged to visit our office in New York to meet with our curators and prepare installation plans. Travel to our New York office for curatorial meetings is not part of Moving Walls payment and is not a requirement. Photographers who are unable to travel to New York may correspond with our curators by e-mail and skype and submit their installation plans electronically."]
            [:p "While curators work closely with photographers to determine an installation plan, final curatorial decisions are at the discretion of the Moving Walls curators and selection committee."]
 
            [:p "During this time, the selected photographers will be invited to apply for the Emerging Photographer Travel Grant."]
@@ -159,16 +159,16 @@
            [:p "Yes, you are welcome to submit multiple entries. However, you will need to submit separate applications for each."]
 
            [:h3 "I am not a professional photographer, am I eligible to submit work?"]
-           [:p "You do not need to be a professional photographer to apply. Submissions are reviewed based on the quality of the images; the coherence and storytelling capabilities of the project; and relevance of the topic for the Open Society Foundations. We place value first and foremost on the quality of the images, not the photographer's biography. Please note that the selection process is quite competitive (in spring 2011, we received approx. 400 submissions) and your work will be judged against that of professional photographers."]
+           [:p "You do not need to be a professional photographer to apply, however, submissions are reviewed based on the quality of the images. The coherence and storytelling capabilities of the project, and relevance of the topic for the Open Society Foundations, are also considered. We place value first and foremost on the quality of the images, not the photographer's biography. Please note that the selection process is quite competitive (in Spring 2011, we received approx. 400 submissions) and your work will be judged against that of professional photographers."]
 
            [:h3 "I am a curator/part of a photography collective. Do you accept submissions for group exhibitions?"]
            [:p "Group submissions are eligible. It is important to consider, however, that a group application will be considered for a space normally devoted to a single photographer's work. The edits will need to be quite tight in order to accommodate a group exhibition."]
 
            [:h3 "Do you accept photographic projects about the United States?"]
-           [:p "Yes. The Open Society Foundations has extensive programming based in the United States. Please consult the US Programs website to get a sense of the priority issues: http://www.soros.org/initiatives/usprograms."]
+           [:p "Yes. The Open Society Foundations has extensive programming based in the United States. Please consult the US Programs website to get a sense of the priority issues: " (link-to "http://www.soros.org/initiatives/usprograms" "http://www.soros.org/initiatives/usprograms") "."]
 
            [:h3 "Is there work that you do not show?"]
-           [:p "The work exhibited should explore issues in which the larger Open Society Foundations engages. Please take a look at our website (www.soros.org) for a better idea of what those issues are. There are some geographic regions in which we do not currently work, including Oceania, India, and Canada ; work from these countries will likely be considered out of scope. Themes that are out of scope include animal rights  and images of extreme poverty or homelessness without a social justice component ."]
+           [:p "The work exhibited should explore issues in which the larger Open Society Foundations engages. Please take a look at our website (" (link-to "http://www.soros.org/" "www.soros.org") ") for a better idea of what those issues are. There are some geographic regions in which we do not currently work, including Oceania, India, and Canada ; work from these countries will likely be considered out of scope. Themes that are out of scope include animal rights  and images of extreme poverty or homelessness without a social justice component."]
 
            [:p "We do not show photographs that have been staged or manipulated, nor do we exhibit bodies of work documenting the activities of NGOs. Work made by journalists on a short-term assignment will be considered, but longer-term projects are strongly preferred."]
 
@@ -176,6 +176,10 @@
            [:p "You are welcome to apply with a body of work related to a story that has been told in a previous Moving Walls exhibition. However, it is unlikely that it will be selected soon afterwards, unless the storytelling method and photographic style differ significantly."]
 
            [:h3 "Help, it's the day of the deadline and the application site appears to have crashed."]
-           [:p "Due to high volume of submissions, we occasionally experience technical difficulties with the submission system on or approaching the day of the deadline. If this occurs, we ask that you please be patient and continue to check the site. If troubles persist, please send an email to docphoto@sorosny.org."]
+           [:p "Due to high volume of submissions, we occasionally experience technical difficulties with the submission system on or approaching the day of the deadline. If this occurs, we ask that you please be patient and continue to check the site. If troubles persist, please send an email to " (link-to "mailto:docphoto@sorosny.org" "docphoto@sorosny.org") "."]
 
-           )})
+           )
+   :central-asia (list
+                  [:div {:style "text-align: center"}
+                   ]
+   )})
