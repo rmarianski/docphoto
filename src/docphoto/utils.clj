6f9d9f98e6f32e6a68d1c562253e8744762fc4d0
Robert Marianski <rob@marianski.com>
2012-03-19 21:13:30 -0400

    remove reflection calls from utils

--- src/docphoto/utils.clj
+++ src/docphoto/utils.clj
@@ -31,7 +31,7 @@
     `(defn-memo ~@args)
     `(defn ~@args)))
 
-(defn md5 [s] (DigestUtils/md5Hex s))
+(defn md5 [^String s] (DigestUtils/md5Hex s))
 
 (defn lorem-ipsum []
   "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
