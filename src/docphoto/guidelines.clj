07aeeb3d4f4375f7ea3fc1f7b7cddbc9b6a8a10d
Robert Marianski <rob@marianski.com>
2012-12-21 13:26:31 -0500

    updated cac 2013 deadline

--- src/docphoto/guidelines.clj
+++ src/docphoto/guidelines.clj
@@ -60,13 +60,13 @@
     (list
      [:p "The Open Society Documentary Photography Project and the Arts and Culture Program are offering grants for documentary photographers from Central Asia, the South Caucasus, Afghanistan, Mongolia, and Pakistan. With these grants, we support visual documentation of important human rights and social issues in the region and provide training and mentorship to local photographers.  For more details and the application instructions, please visit: " (link "http://www.soros.org/initiatives/photography/focus_areas/production-individual/guidelines")]
      [:p "If you have any questions, please contact Anna Overstrom-Coleman, Program Assistant, at 212-506-0009 or " (link-to "mailto:aoverstrom-coleman@sorosny.org" "aoverstrom-coleman@sorosny.org") "."]
-     [:p "CAC 2013 deadline: Tuesday, February 12, 2013, 5 p.m. EST."]
+     [:p "CAC 2013 deadline: Tuesday, March 5, 2013, 5 p.m. EST."]
      [:p "To apply, please visit: " (link "http://docphoto.soros.org/2013/apply")])
     :ru
     (list
      [:p "«Проект в области документальной фотографии» и «Программа по искусству и культуре» прелагают гранты для фотографов-документалистов из Центральной Азии, Южного Кавказа, Афганистана, Монголии и Пакистана. При помощи данных грантов мы поддерживаем визуальное документирование  важных вопросов прав человека и социальных тем региона и предоставляем тренинг и наставническую профессиональную поддержку местным фотографам.  Более подробная информация и инструкции по оформлению заявки – пожалуйста, посетите вебсайт: " (link "http://www.soros.org/initiatives/photography/focus_areas/production-individual/guidelines")]
      [:p "Если у Вас возникли вопросы, пожалуйста, обращайтесь к Анне Overstrom-Коулман, ассистент программы, в 212-506-0009 или " (link-to "mailto:aoverstrom-coleman@sorosny.org" "aoverstrom-coleman@sorosny.org") "."]
-     [:p "CAC 2013 срок: Вторник, 12 Февраля 2013, 5 вечера EST."]
+     [:p "CAC 2013 срок: Вторник, 5 март 2013, 5 вечера EST."]
      [:p "Чтобы подать заявку, пожалуйста, посетите: " (link "http://docphoto.soros.org/2013/apply")])}
 
    })
